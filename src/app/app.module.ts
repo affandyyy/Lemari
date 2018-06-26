5be6990e81ced13da6799da0e4f605d71e092b21
affandyyy <syahmiaffandy@gmail.com>
2018-06-26 14:12:55 +0800

    Fix login issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ firebase.initializeApp(FIREBASE_CREDENTIALS)
     HomePage,
     TabsPage,
     EditPage,
+    LoginPage
     // IntroPage
   ],
   imports: [
@@ -64,6 +65,7 @@ firebase.initializeApp(FIREBASE_CREDENTIALS)
     HomePage,
     TabsPage,
     EditPage,
+    LoginPage
     // IntroPage
   ],
   providers: [
